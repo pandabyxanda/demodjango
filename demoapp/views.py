d0f2ac85f7535ea6f8b6a0a52731f5e3caab3240
pandabyxanda <110741053+pandabyxanda@users.noreply.github.com>
2024-01-05 22:48:44 +0800

    Update views.py

--- demoapp/views.py
+++ demoapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {"data":"Home Page of Django App, testing CD"}
+    context = {"data":"Home Page of Django App, testing CD, lol haha"}
     return render(request,'demoapp/index.html', context)
